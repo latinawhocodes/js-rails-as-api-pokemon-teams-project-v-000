8d5b0fb7b27c29d4abb80dfe19db53719c457128
latinawhocodes <alyssaroque01@gmail.com>
2020-04-25 19:20:26 -0400

    Update pokemons_controller.rb

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,3 +1,18 @@
 class PokemonsController < ApplicationController
 
+    def index
+        pokemons = Pokemon.all
+        render json: pokemons
+    end
+
+    def show
+        pokemon = Pokemon.find(params[:id])
+        render json: pokemon
+    end
+
+    def create
+    end
+
+    def destroy
+    end
 end
